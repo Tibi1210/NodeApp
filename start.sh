5ca904f73e455c1f87e641c763f71508bf079e6e
Vajda Tibor <h053353@stud.u-szeged.hu>
2024-12-04 13:03:09 +0100

    auto build test1

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Start rsyslog
 rsyslogd
-# test commit for auto build
+# test commit for auto build1
 # Start your app with PM2
 pm2-runtime start dist/index.js --output /var/log/pm2/out.log --error /var/log/pm2/error.log
